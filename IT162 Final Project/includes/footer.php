30798778b431847501469aaf81c01662420c3192
aymenk123 <97873885+aymenk123@users.noreply.github.com>
2022-12-17 23:44:08 -0800

    Add files via upload

---
+++ IT162 Final Project/includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- START Footer -->
+  <footer>
+      <p><small>&copy; 2022-<?=date("Y")?> by 
+          <a href="contact.php">Aymen Khaleefa</a>, All Rights Reserved ~ 
+          <a id="html-checker" href="#">Check HTML</a> ~ 
+          <a id="css-checker" href="#">Check CSS</a></small>
+     </p>
+    </footer>
+<!-- END Footer --> 
---
+++ IT162 Final Project/includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <meta charset="UTF-8">
+    <title>YuYu Collection</title>
+     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+     </head>
+    <body>
+        
+        
+       <div class="header">
+         <div class="container">
+            <div class="navbar">
+         <div class="logo">
+        <img src="images/OIP%20(1).jpg">
+        </div>
+        <nav>
+            <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Products</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Account</a></li>
+
+
+            </ul>
+            </nav>
+            <img src="images/shopping-cart-svg-png-icon-download-28.png" width="30px" height="30px">
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <h1>Clothing Shop<br> Online</h1>
+                <p>Success isn't always about greatness. It's about consistency. Consistent<br> hard work leads to success. Greatness will come.</p>
+                <a href="" class="btn">Explore Now &#8594 </a>
+            </div>
+            <div class ="col-2">
+                <img src="images/confidence-is-the-best-accessory.jpg">
+              </div>
+            </div>
+        </div>
+        </div>
